3d9f06ea6b77763e905d9ff4a37b1c04962ce0c8
jrichardsz <jrichardsz.java@gmail.com>
2020-04-19 19:23:38 -0500

    link start

---
+++ src/linkstartjs-nerve-utils.js
@@ -0,0 +1,12 @@
+/*!
+ * linkstartjs-nerve-utils
+ * Copyright(c) 2018-2020 JRichardsz
+ */
+
+'use strict';
+
+/**
+ * Expose utils
+ */
+
+exports.LinksStartJsDomUtil = require('./linkstartjs/dom');
---
+++ src/linkstartjs/dom/index.js
@@ -0,0 +1,138 @@
+'use strict';
+
+function LinksStartJsDomUtil() {}
+
+LinksStartJsDomUtil.render = function(page) {
+  let frag = document.createRange().createContextualFragment(page.getHtml());
+  return frag;
+}
+
+LinksStartJsDomUtil.applyActionBindings = function(page, actionInstance) {
+
+  let actionableElements = page.getActionableElements();
+  if (typeof actionableElements === 'undefined' || actionableElements.length == 0) {
+    return;
+  }
+
+  actionableElements.forEach(function(actionableElement) {
+    let tagId = actionableElement.tagId;
+    let lsId = actionableElement.lsId;
+
+    if (tagId && lsId) {
+      let domElement = getElementByLsId(lsId);
+      let onclickFunction = getDefaultActionForElement(actionInstance, tagId);
+      if (domElement && onclickFunction) {
+        domElement.onclick = onclickFunction;
+      }
+    }
+  });
+
+}
+
+LinksStartJsDomUtil.applyModelBindings = function(page) {
+
+  var model = {};
+
+  let modelElements = page.getModelElements();
+  if (typeof modelElements === 'undefined' || modelElements.length == 0) {
+    return;
+  }
+
+  modelElements.forEach(function(modelElement) {
+    let tagId = modelElement.tagId;
+    let lsId = modelElement.lsId;
+
+    if (tagId && lsId) {
+      let domElement = getElementByLsId(lsId);
+      if (domElement.tagName === 'INPUT') {
+
+        domElement.addEventListener("change", function(event) {
+          if(domElement.type === 'text'){
+            model[tagId] = event.target.value;
+          }else if(domElement.type === 'radio'){
+            model[domElement.name] = event.target.value;
+          }else if(domElement.type === 'checkbox'){
+            if(domElement.checked === true){
+              model[tagId] = true;
+            }else{
+              model[tagId] = false;
+            }
+          }
+        });
+        //set default value
+
+        if(domElement.type === 'text'){
+          model[tagId] = domElement.value;
+        }else if(domElement.type === 'radio'){
+          let radioValue = getDefaultRadioValue(domElement);
+          if(typeof radioValue !== 'undefined'){
+            model[domElement.name] = radioValue;
+          }
+        }else if(domElement.type === 'checkbox'){
+          let checkboxValue = getDefaultCheckboxValue(domElement);
+          model[tagId] = checkboxValue;
+        }
+
+      } else if (domElement.tagName === 'SELECT') {
+        domElement.addEventListener("change", function(event) {
+          model[tagId] = event.target.value;
+        });
+        //set default value
+        model[tagId] = getDefaultSelectValue(domElement);
+      } else {
+        console.log("Model binding is not implemented yet:" + domElement.tagName);
+      }
+    }
+  });
+
+  return model;
+}
+
+function getDefaultCheckboxValue(checkboxElement) {
+  if(checkboxElement.checked === true){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function getDefaultRadioValue(radioElement) {
+  if(radioElement.checked === true){
+    return radioElement.value;
+  }
+}
+
+function getDefaultSelectValue(selectElement) {
+  var myOptions = selectElement.options;
+  for (var i = 0; i < myOptions.length; i++) {
+    var isDefSelected = myOptions[i].selected;
+    if (isDefSelected) {
+      return myOptions[i].value;
+    }
+  }
+}
+
+function getElementByLsId(lsId) {
+  let list = document.querySelectorAll('[ls-id="' + lsId + '"]');
+  if (list.length == 1) {
+    return list[0];
+  } else {
+    console.log("There are not any element or there are more than one:" + lsId);
+  }
+}
+
+function getDefaultActionForElement(actionInstance, tagId) {
+
+  let objectPropertyNames = Object.getOwnPropertyNames(actionInstance);
+
+  for (let objectPropertyName of objectPropertyNames) {
+    if (typeof actionInstance[objectPropertyName] !== 'function') {
+      continue;
+    }
+    if (objectPropertyName.startsWith(tagId)) {
+      return actionInstance[objectPropertyName];
+    }
+  }
+}
+
+module.exports = LinksStartJsDomUtil;
